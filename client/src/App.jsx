6177a7da34eb256000ff6640bc1347a6e1e20032
Monae Curbeam <curbeammonae@gmail.com>
2024-03-09 16:33:00 -0500

    create heade component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />}/>
-      <Route path="/signin" element={<SignIn />}/>
-      <Route path="/signup" element={<SignUp />}/>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div>
+    
+      <Link to="/">
+        <li className="hidden sm:inline text-slate-700 hover:underline">
+          Home
+        </li>
+      </Link>
+      <Link to="/about">
+        <li className="hidden sm:inline text-slate-700 hover:underline">
+          About
+        </li>
+      </Link>
+      <Link to="/signin">
+        <li className=" text-slate-700 hover:underline">Sign In</li>
+      </Link>
+      <Link to="/signup">
+        <li className=" text-slate-700 hover:underline">Sign Up</li>
+      </Link>
+    </div>
+  );
+}
