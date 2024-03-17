c2e4a34e47f96e99456c84619b2d7499bde783b0
Monae Curbeam <curbeammonae@gmail.com>
2024-03-17 09:59:17 -0400

    create search api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createPost, deletePost, updatePost, getPost } from '../controllers/post.controller.js';
+import { createPost, deletePost, updatePost, getPost, getAllPosts } from '../controllers/post.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/create', verifyToken, createPost)
 router.delete('/delete/:id', verifyToken, deletePost)
 router.post('/update/:id', verifyToken, updatePost)
 router.get('/get/:id', getPost)
+router.get('/get', getAllPosts)
 
 export default router;
