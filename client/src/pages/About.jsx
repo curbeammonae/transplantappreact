c9cd9f5836b11e83f6c255a1c0d64414bf29ade8
monaecurbeam98 <curbeammonae@gmail.com>
2024-05-08 14:30:38 -0400

    responsiveness

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div><p className='text-[50px] text-center text-white'>
+    <div><p className='max-[1200px]:text-[20px] text-[50px] text-center text-white'>
     Welcome to TransPLANT, your ultimate destination for embracing the vibrant world of plant-based eating! Whether you're a curious newcomer or a seasoned enthusiast, our platform offers a an abundance of resources to empower your journey towards a healthier, more sustainable lifestyle. Discover delicious recipes, practical tips, and inspiring stories to fuel your transition to plant-powered living, all while nurturing your body, mind, and the planet</p></div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,11 +5,11 @@ import foodone from '../assets/images/food1.png'
 export default function Home() {
   return (
     <div className=''>
-      <section className='max-[600px]:flex-col flex gap-10'>
-      <h1 className='max-[600px]:text-[40px]   text-center text-[70px] m-10 text-[#FEF2E6]'>Welcome to TransPLANT. A site designed for people interested in a plant based diet.</h1>
+      <section className='max-[1200px]:flex-col flex gap-10'>
+      <h1 className='max-[1200px]:text-[40px]   text-center text-[70px] m-10 text-[#FEF2E6]'>Welcome to TransPLANT. A site designed for people interested in a plant based diet.</h1>
       
       </section>
-      <section className='bg-[#FEF2E6] h-[20rem] pt-5'>
+      <section className='max-[1200px]:h-[25rem] bg-[#FEF2E6] h-[20rem] pt-5'>
         <h2 className='max-[600px]:text-[30px]  text-[#126F63] text-[62px] text-center'>Changing Lives One Recipe At A Time.</h2>
         
         
